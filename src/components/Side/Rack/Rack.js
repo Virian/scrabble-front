8ae47098a720ad794befc07fc903e547b14e4e90
Grzegorz Stokłosa <gstok@poczta.fm>
2020-01-14 01:35:16 +0100

    Tile rack styling

--- src/components/Side/Rack/Rack.js
+++ src/components/Side/Rack/Rack.js
@@ -3,15 +3,24 @@ import Tile from '../../Tile';
 
 export default function Rack({ tiles }) {
   return (
-    <div className="rack">
-      {tiles.map((tile, index) => (
-        <Tile
-          key={`rack-tile-${index}`}
-          letter={tile.letter}
-          score={tile.score}
-          movable
-        />
-      ))}
+    <div className="rack-wrapper">
+      <div className="rack">
+        {Array(7).fill(0).map((_, index) => (
+          <div
+            key={`rack-tile-wrapper-${index}`}
+            className="rack__tile-wrapper"
+          >
+            {tiles[index] && (
+              <Tile
+                key={`rack-tile-${index}`}
+                letter={tiles[index].letter}
+                score={tiles[index].score}
+                movable
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   )
 };
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -4,7 +4,7 @@ export default function Tile({ letter, score, movable }) {
   return (
     <div className={`tile ${movable ? 'tile--movable' : ''}`}>
       <span className="tile__letter">{letter}</span>
-      <span className="tile__score">{score}</span>
+      {score ? <span className="tile__score">{score}</span> : null}
     </div>
   )
 };
