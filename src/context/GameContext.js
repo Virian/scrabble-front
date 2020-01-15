19f9c0e62009bac5becf4aa1618d0b92c7736ecb
Grzegorz Stokłosa <gstok@poczta.fm>
2020-01-15 22:37:39 +0100

    Set window title depending on player turn

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import GameState from '../enum/GameState';
 
 export const GameContext = React.createContext(null);
@@ -7,6 +7,14 @@ export const GameContextProvider = ({ children }) => {
   const [gameState, setGameState] = useState(GameState.WAITING_GAME_START);
   const [userId, setUserId] = useState(null);
   const [activePlayerIndex, setActivePlayerIndex] = useState(-1);
+
+  useEffect(() => {
+    if (gameState === GameState.PLAYING || gameState === GameState.PLAYING_FIRST_TURN) { // TODO: later there may be more game states to consider
+      document.title = '\u25b6Scrabble'
+    } else {
+      document.title = 'Scrabble'
+    }
+  }, [gameState])
 
   return (
     <GameContext.Provider value={{
