b4111fbdc5f194fc3d3755e7c68b370b29cd709e
Virian <gstok@poczta.fm>
2020-01-18 21:01:35 +0100

    Highlight rack slots on hovering with a tile

--- src/components/Side/Rack/RackSlot/RackSlot.js
+++ src/components/Side/Rack/RackSlot/RackSlot.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import classNames from 'classnames';
 import { useDrop } from 'react-dnd';
 import ItemTypes from '../../../../enum/ItemTypes';
 import Tile from '../../../Tile';
@@ -10,13 +11,20 @@ export default function Rack({
   toggleTileHighlight
 }) {
   const ref = useRef(null);
-  const [, drop] = useDrop({
+  const [{ hovered }, drop] = useDrop({
     accept: ItemTypes.TILE,
     hover(item, monitor) {
-      if (!ref.current) return;
+      if (!ref.current) {
+        return;
+      }
+      if (!item.isRackTile) {
+        return;
+      }
       const dragIndex = item.index;
       const hoverIndex = index;
-      if (dragIndex === hoverIndex) return;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
       // Determine rectangle on screen
       const hoverBoundingRect = ref.current.getBoundingClientRect();
       const horizontalLeftBreakpoint = (hoverBoundingRect.right - hoverBoundingRect.left) / 4;
@@ -29,21 +37,28 @@ export default function Rack({
       // When dragging right, only move when the cursor passed 25% of item width
       // When dragging left, only move when the cursor passed 25% of item width
       // Dragging right
-      if (dragIndex < hoverIndex && hoverClientX < horizontalLeftBreakpoint) return;
+      if (dragIndex < hoverIndex && hoverClientX < horizontalLeftBreakpoint) {
+        return;
+      }
       // Dragging left
-      if (dragIndex > hoverIndex && hoverClientX > horizontalRightBreakpoint) return;
+      if (dragIndex > hoverIndex && hoverClientX > horizontalRightBreakpoint) {
+        return;
+      }
       moveRackTiles(dragIndex, hoverIndex);
       // Note: we're mutating the monitor item here! Generally it's better to avoid mutations,
       // but it's good here for the sake of performance to avoid expensive index searches.
       item.index = hoverIndex;
     },
+    collect: monitor => ({
+      hovered: monitor.isOver(),
+    }),
   });
   drop(ref);
 
   return (
     <div
       ref={ref}
-      className="rack-slot"
+      className={classNames('rack-slot', { 'rack-slot--highlighted' : hovered })}
     >
       {tile && (
         <Tile
